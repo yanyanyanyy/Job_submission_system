82f071603987a20d2bdf1fa232946c5cc6165de7
brinenick511 <101073680+brinenick511@users.noreply.github.com>
2023-05-27 17:44:53 +0800

    Update GitFileUpdater.cs

--- JobSubmissionSystem/webapi/Data/GitFileUpdater.cs
+++ JobSubmissionSystem/webapi/Data/GitFileUpdater.cs
@@ -1,79 +1,99 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Xml;
+using LibGit2Sharp;
 using Newtonsoft.Json;
-
-public class CR//CommitRecord
-{
-    //public Student? student { get; set; }
-    // 提交记录所属学生
-    public int index { get; set; } = 0;
-    // 提交所属的作业序号，例如第一次作业，第二次作业
-    //public DateTime? created { get; set; }
-    //作业提交时间
-    public DateTime? updated { get; set; }
-    //作业修改时间
-    //public bool modified { get; set; }
-    //作业从上次查询至今是否有修改
-    //public bool isCorrected { get; set; }
-    // 是否批改过
-
-}
-public class F
-{
-    public string Path { get; set; } // 文件路径及文件名
-    public DateTime Updated { get; set; } // 对此文件最后的操作时间，操作包括上传、创建、更新修改等等
-    public int idx { get; set; } // 文件所属的作业号
-}
-
-public class Commit
-{
-    public string Sha { get; set; }
-    public CommitInfo CommitInfo { get; set; }
-}
 
 public class CommitInfo
 {
-    [JsonProperty("commit")]
-    public CommitDetails Commit { get; set; }
+    // /commits.json获取的是List<CommitInfo>
+    [JsonProperty("url")] public string url { get; set; }
+    [JsonProperty("commit")] public CommitCommit commit { get; set; }
 }
-
-public class CommitDetails
+public class CommitCommit
 {
-    [JsonProperty("author")]
-    public CommitAuthor Author { get; set; }
-
-    [JsonProperty("committer")]
-    public CommitAuthor Committer { get; set; }
+    [JsonProperty("author")] public CommitAuthor author { get; set; }
 }
-
 public class CommitAuthor
 {
-    [JsonProperty("date")]
-    public string Date { get; set; }
+    [JsonProperty("date")] public string date { get; set; }
 }
 
-public class GitHubApiResult
+public class Url
 {
-    public List<GitHubFileInfo> Tree { get; set; }
+    [JsonProperty("files")] public List<UrlFile> files { get; set; }
+
+}
+public class UrlFile
+{
+    [JsonProperty("filename")] public string filename { get; set; }
 }
 
-public class GitHubFileInfo
-{
-    public string Path { get; set; }
-    public string Type { get; set; }
-    public string Sha { get; set; }
-}
+
+
 
 public class Program
 {
+    public const string timeFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    private static int GetAssignmentsIndex(string input)
+    {
+        // 检查字符串长度是否满足要求
+        if (input.Length < 3)
+        {
+            return -1;
+        }
+
+        // 检查字符串中是否包含 '/'
+        int slashIndex = input.IndexOf('/');
+        if (slashIndex == -1)
+        {
+            return -1;
+        }
+
+        // 从最左端的 '/' 往左截取数字部分
+        int numberPart = 0;
+        int multiplier = 1;
+        for (int i = slashIndex - 1; i >= 0; i--)
+        {
+            char currentChar = input[i];
+            if (currentChar >= '0' && currentChar <= '9')
+            {
+                numberPart = numberPart + ((currentChar - '0') * multiplier);
+                multiplier *= 10;
+            }
+            else
+            {
+                break;
+            }
+        }
+        // 返回截取到的数字
+        return numberPart;
+    }
+    private static string GetOwnerAndRepoFromUrl(string url)
+    {
+        // 从URL中提取仓库所有者和仓库名
+        var startIndex = url.IndexOf(".com/") + 5;
+        var endIndex = url.LastIndexOf(".git") == -1 ? url.LastIndexOf("") + 1 : url.LastIndexOf(".git");
+        return url.Substring(startIndex, endIndex - startIndex);
+    }
+    private static async Task<Url> GetUrlInfo(HttpClient httpClient, string commitInfoUrl)
+    {
+        // 对每一次提交-查询URL-要查的是filename
+        var response = await httpClient.GetAsync(commitInfoUrl);
+        response.EnsureSuccessStatusCode();
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var url = JsonConvert.DeserializeObject<Url>(responseContent);
+        return url;
+    }
     public static async Task<List<F>> GetFileInformation(string addr, string token)
     {
         // 构建请求地址
-        var apiUrl = $"https://api.github.com/repos/{GetOwnerAndRepoFromUrl(addr)}/git/trees/main?recursive=1";
+        //var apiUrl = $"https://api.github.com/repos/{GetOwnerAndRepoFromUrl(addr)}/git/trees/main?recursive=1";
+        var apiUrl = $"https://api.github.com/repos/{GetOwnerAndRepoFromUrl(addr)}/commits";
 
         // 创建HttpClient对象，并设置Headers
         using (var httpClient = new HttpClient())
@@ -89,118 +109,81 @@ public class Program
             var responseContent = await response.Content.ReadAsStringAsync();
 
             // 解析响应内容
-            var apiResult = JsonConvert.DeserializeObject<GitHubApiResult>(responseContent);
+            var apiResult = JsonConvert.DeserializeObject<List<CommitInfo>>(responseContent);
 
             // 获取提交信息
-            var commitInfoTasks = new List<Task<Commit>>();
-            foreach (var file in apiResult.Tree)
+            var commitInfoTasks = new List<Task<Url>>();
+            var timeList = new List<string>();
+            foreach (var commitInfo in apiResult)
             {
-                if (file.Type == "blob") // 只获取文件的提交信息
-                {
-                    var commitInfoUrl = $"https://api.github.com/repos/{GetOwnerAndRepoFromUrl(addr)}/commits?path={file.Path}&per_page=1";
-                    commitInfoTasks.Add(GetCommitInfo(httpClient, commitInfoUrl));
-                }
+                var commitInfoUrl = commitInfo.url;
+                commitInfoTasks.Add(GetUrlInfo(httpClient, commitInfoUrl));
+                timeList.Add(commitInfo.commit.author.date);
             }
 
             await Task.WhenAll(commitInfoTasks);
 
             // 处理文件信息
-            var fileInfoList = new List<F>();
-            foreach (var file in apiResult.Tree)
+            List<F> files = new List<F>();
+            for (var i = 0; i < timeList.Count; i++)
             {
-                if (file.Type == "blob") // 只处理文件
+                DateTime tmpTime = DateTime.ParseExact(timeList[i], timeFormat, null);
+                foreach (var filename in commitInfoTasks[i].Result.files)
                 {
-                    var fileInfo = new F
+                    string tmpPath = filename.filename;
+                    int AssIdx = GetAssignmentsIndex(tmpPath);
+                    if (AssIdx == -1)
                     {
-                        Path = file.Path,
-                        Updated = GetFileUpdatedTime(commitInfoTasks, file.Path),
-                        idx = GetNumberFromSubstring(file.Path)
-                };
-                    fileInfoList.Add(fileInfo);
+                        continue;
+                    }
+                    // 我需要在这里查询files中是否有这样一个file，如果
+                    int idx = files.FindIndex(f => f.Path == tmpPath);
+                    if (idx == -1)
+                    {
+                        // 这个文件没有出现过
+                        files.Add(new F
+                        {
+                            idx = AssIdx,
+                            Path = tmpPath,
+                            times = new List<DateTime> { tmpTime }
+                        });
+                    }
+                    else
+                    {
+                        files[idx].times.Add(tmpTime);
+                    }
+
                 }
             }
-
-            return fileInfoList;
+            return files;
         }
     }
-
-    private static async Task<Commit> GetCommitInfo(HttpClient httpClient, string commitInfoUrl)
+    public static List<CR> GetAssignments(string addr,string token)
     {
-        var response = await httpClient.GetAsync(commitInfoUrl);
-        response.EnsureSuccessStatusCode();
 
-        var responseContent = await response.Content.ReadAsStringAsync();
-        var commits = JsonConvert.DeserializeObject<List<Commit>>(responseContent);
-
-        return commits.Count > 0 ? commits[0] : null;
-    }
-
-    private static DateTime GetFileUpdatedTime(List<Task<Commit>> commitInfoTasks, string filePath)
-    {
-        foreach (var task in commitInfoTasks)
-        {
-            // 等待任务完成
-            task.Wait();
-
-            var commit = task.Result;
-            //if(commit.CommitInfo!=null) Console.WriteLine("1");
-            if (commit != null && commit.CommitInfo != null && commit.CommitInfo.Commit != null && commit.CommitInfo.Commit.Author != null && !string.IsNullOrEmpty(commit.CommitInfo.Commit.Author.Date))
-            {
-                if (DateTime.TryParse(commit.CommitInfo.Commit.Author.Date, out DateTime updatedTime))
-                {
-                    return updatedTime;
-                }
-            }
-        }
-
-        // 如果没有更新时间，则使用创建时间
-        return DateTime.MinValue;
-    }
-
-
-    private static string GetOwnerAndRepoFromUrl(string url)
-    {
-        // 从URL中提取仓库所有者和仓库名
-        var startIndex = url.IndexOf(".com/") + 5;
-        var endIndex = url.LastIndexOf(".git");
-        return url.Substring(startIndex, endIndex - startIndex);
-    }
-
-    public static int GetNumberFromSubstring(string str, int startIndex=11, int length=2)
-    {
-        if (startIndex + length > str.Length)
-        {
-            // 子串长度超过字符串长度，返回 -1
-            return -1;
-        }
-        string sub = str.Substring(startIndex, length);
-        int number;
-        if (int.TryParse(sub, out number))
-        {
-            // 子串是数字，返回解析后的整数值
-            return number;
-        }
-        // 子串不是数字，返回 -1
-        return -1;
-    }
-    public static List<CR> GetLatestUpdate(string addr,string token)
-    {
         var list = new List<CR>();
         var result = GetFileInformation(addr, token).GetAwaiter().GetResult();
         foreach (var file in result)
         {
-            //Console.WriteLine($"Path: {file.Path}, Updated: {file.Updated}, Idx: {file.idx}");
+            // 只处理作业文件
             if (file.idx == -1) continue;
+            file.disp();
+            var created = file.times.Min();
+            var updated = file.times.Max();
             bool flag = true;
-            foreach(var cr in list)
+            foreach (var cr in list)
             {
                 if (cr.index == file.idx)
                 {
-                    if (cr.updated == null|| cr.updated < file.Updated)
-                    {
-                        cr.updated = file.Updated;
-                    }
                     flag = false;
+                    if (cr.updated == null || cr.updated < updated)
+                    {
+                        cr.updated = updated;
+                    }
+                    if (cr.created == null || cr.created > updated)
+                    {
+                        cr.created = updated;
+                    }
                 }
             }
             if (flag)
@@ -208,26 +191,61 @@ public class Program
                 list.Add(new CR
                 {
                     index = file.idx,
-                    updated = file.Updated
+                    created = updated,
+                    updated = updated
                 });
             }
+
         }
         return list;
     }
     public static void Main(string[] args)
     {
         // 测试示例
-        string addr = "https://github.com/brinenick511/test.git";
-        string token = "请使用自己的token测试（token作为代码上传到git后会失效）";
+        string addr = "https://github.com/brinenick511/test";
+        string token = "";
 
-        var ans = GetLatestUpdate(addr, token);
-
-        foreach(var cr in ans)
+        var ans = 
+            GetAssignments(addr,token);
+        foreach(var a in ans)
         {
-            Console.WriteLine($"CR.index: {cr.index}, CR.updated: {cr.updated}");
+            a.disp();
         }
 
-        Console.WriteLine("Finished");
+        Console.WriteLine("\nFinished");
         Console.ReadKey();
+    }
+}
+public class CR
+{
+    //public Student? student { get; set; }
+    // 提交记录所属学生
+    public int index { get; set; } = -1;
+    // 提交所属的作业序号，例如第一次作业，第二次作业
+    public DateTime? created { get; set; }
+    //作业提交时间
+    public DateTime? updated { get; set; }
+    //作业修改时间
+    //public bool modified { get; set; }
+    //作业从上次查询至今是否有修改
+    //public bool isCorrected { get; set; }
+    // 是否批改过
+    public void disp()
+    {
+        Console.WriteLine($"\nCR.index: {index}, Cr.created: {created}, CR.updated: {updated}");
+    }
+}
+public class F
+{
+    public string Path { get; set; } // 文件路径及文件名
+    public List<DateTime> times { get; set; } // 每一次修改的时间
+    public int idx { get; set; } // 文件所属的作业号
+    public void disp()
+    {
+        Console.WriteLine($"\nPath: {Path}, Idx: {idx}");
+        foreach (var times in times)
+        {
+            Console.WriteLine(times.ToString());
+        }
     }
 }
