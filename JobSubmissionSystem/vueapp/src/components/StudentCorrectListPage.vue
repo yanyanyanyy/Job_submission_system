ed155afd7955b9fcca796c95eb7a2f886141766d
W-Vicky11 <126785491+W-Vicky11@users.noreply.github.com>
2023-05-31 18:31:03 +0800

    Add files via upload

--- JobSubmissionSystem/vueapp/src/components/StudentCorrectListPage.vue
+++ JobSubmissionSystem/vueapp/src/components/StudentCorrectListPage.vue
@@ -1,18 +1,22 @@
 <template>
     <!--本页面为学生点击查看老师评语按钮后的页面-->
+    <div class="studentCorrectList">
   
-      <h1>欢迎！{{ username }}</h1>
+        <h1>欢迎！{{ username }}</h1>
       
-      
-      <h2>请查看您的作业提交情况</h2>
-      <el-table :data="Assignment" border style="width: 100%">
+        <h2>请查看您的作业提交情况!</h2>
+
+        <el-table :data="Assignment" border height="400" style="width: 100%" >
         <el-table-column prop="num" label="作业号" width="180" />
         <el-table-column prop="state" label="批改状态" width="180" />
+
       <!--下面这一行的代码应该实现成点击就可以跳转至批改界面的功能，暂时还没做-->
         <el-table-column prop="url" label="查看批改情况" />
     </el-table>
     <!--返回按钮-->
     <el-button type="info">返回上一页</el-button>
+    </div>
+
 </template>
       
 <script>
@@ -34,13 +38,24 @@ export default{
         }
     },
     methods:{
-                 
                     
     }
 } 
 </script>
       
-<style scoped>
-      
+<style lang="scss" scoped>
+    .studentCorrectList{
+        text-align:center;
+        position:absolute;
+        width: 100%;
+        height: 100%;
+        background-image: url('/images/svgs/login-bg.svg');
+    }
+    h1,h2{
+        color:#f9cdaa;
+    }
+    .el-button{
+        float:right;
+    }
 </style>
       
